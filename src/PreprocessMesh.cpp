ee38226683992162158e0a259abd6d8a99b1a9fd
Balamurugan Marimuthu <bmarimut@ece024.ece.local.cmu.edu>
2021-11-26 15:56:56 -0500

    clean build

--- src/PreprocessMesh.cpp
+++ src/PreprocessMesh.cpp
@@ -34,7 +34,7 @@ void SampleFromSurface(
     auto it_vert_indices = object.second.attributes.find("vertex_indices");
     if (it_vert_indices != object.second.attributes.end()) {
       pangolin::Image<uint32_t> ibo =
-          pangolin::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
+          std::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
 
       for (int i = 0; i < ibo.h; ++i) {
         linearized_faces.emplace_back(ibo(0, i), ibo(1, i), ibo(2, i));
@@ -43,7 +43,7 @@ void SampleFromSurface(
   }
 
   pangolin::Image<float> vertices =
-      pangolin::get<pangolin::Image<float>>(geom.buffers["geometry"].attributes["vertex"]);
+      std::get<pangolin::Image<float>>(geom.buffers["geometry"].attributes["vertex"]);
 
   for (const Eigen::Vector3i& face : linearized_faces) {
     float area = TriangleArea(
@@ -337,7 +337,7 @@ int main(int argc, char** argv) {
       auto it_vert_indices = object.second.attributes.find("vertex_indices");
       if (it_vert_indices != object.second.attributes.end()) {
         pangolin::Image<uint32_t> ibo =
-            pangolin::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
+            std::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
 
         total_num_faces += ibo.h;
       }
@@ -355,7 +355,7 @@ int main(int argc, char** argv) {
       auto it_vert_indices = object.second.attributes.find("vertex_indices");
       if (it_vert_indices != object.second.attributes.end()) {
         pangolin::Image<uint32_t> ibo =
-            pangolin::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
+            std::get<pangolin::Image<uint32_t>>(it_vert_indices->second);
 
         for (int i = 0; i < ibo.h; ++i) {
           new_ibo.Row(index).CopyFrom(ibo.Row(i));
@@ -378,7 +378,7 @@ int main(int argc, char** argv) {
   // remove textures
   geom.textures.clear();
 
-  pangolin::Image<uint32_t> modelFaces = pangolin::get<pangolin::Image<uint32_t>>(
+  pangolin::Image<uint32_t> modelFaces = std::get<pangolin::Image<uint32_t>>(
       geom.objects.begin()->second.attributes["vertex_indices"]);
 
   float max_dist = BoundingCubeNormalization(geom, true);
